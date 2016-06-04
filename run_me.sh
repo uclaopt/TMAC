aaa5bad9db0795389b35b38f082c8169649d1852
Zhimin Peng <zhiminp@gmail.com>
2016-06-04 10:31:24 -0700

    updated run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -5,12 +5,12 @@ data=""
 epochs=""
 num_threads=""
 label=""
-
+problem_size=""
 
 # function to display welcome message
 function welcome_msg {
     echo "------------------------------------------"
-    echo "|        Welcome to TMAC demos          |"
+    echo "|        Welcome to TMAC demos           |"
     echo "------------------------------------------"
     echo "| Enter 0 to exit                        |"    
     echo "| Enter 1 to run least squares           |"
@@ -18,7 +18,10 @@ function welcome_msg {
     echo "| Enter 3 to run l2 svm                  |"
     echo "| Enter 4 to run l1 logistic regression  |"
     echo "| Enter 5 to run demo PRS                |"   
-    echo "------------------------------------------"    
+    echo "------------------------------------------"
+    echo "| you can download testing data from:    |"
+    echo "|   https://github.com/uclaopt/datasets  |"
+    echo "------------------------------------------"
 }
 
 
@@ -26,7 +29,7 @@ function welcome_msg {
 
 # function to proess the input from the terminal
 function process_app_with_data {
-    echo "Please enter the data file name:"
+    echo "Please enter the data file name:" 
     read data </dev/tty
     echo "Please enter the label file name:"
     read label </dev/tty
@@ -37,7 +40,12 @@ function process_app_with_data {
 }
 
 function process_app_with_no_data {
-    echo "please enter number of epochs"
+    echo "Please enter the problem size:" 
+    read problem_size </dev/tty
+    echo "Please enter the number epochs:"
+    read epochs </dev/tty
+    echo "Please enter the number of threads:"
+    read num_threads </dev/tty
 }
 
 
@@ -79,7 +87,8 @@ while read input </dev/tty; do
 	    exit 0 ;;
 	5 )
 	    app="tmac_prs_demo"
-	    exec="$exec_path$app"
+	    process_app_with_no_data
+	    exec="$exec_path$app -problem_size $problem_size $epochs -nthread $num_threads"
 	    echo "starting TMAC to demo PRS"
 	    $exec
 	    exit 0 ;;
