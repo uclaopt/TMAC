f610fff743ee45ccadb54277ce52cc749f5f4106
Zhimin Peng <zhiminp@gmail.com>
2016-06-07 14:24:23 -0700

    added some comments

--- test/cmp_sync_lasso.cc
+++ test/cmp_sync_lasso.cc
@@ -11,6 +11,13 @@
 #include "algebra_namespace_switcher.h"
 using namespace std;
 
+/*
+  This is the demo code for comparing sync-parallel with async-parallel
+  for solving lasso:
+     min lambda ||x||_1 + 0.5 ||A'x - b||^2
+  where matrix A is a row major matrix.
+ */
+
 double objective(Vector& Atx, Vector& b, Vector& x, double lambda);
 
 int main(int argc, char *argv[]) {
@@ -18,31 +25,32 @@ int main(int argc, char *argv[]) {
   Params params;
   string data_file_name;
   double lambda = 0.;  
-  // Step 1. Parse the input argument
+  // parse data in libsvm format
   parse_input_argv_libsvm(&params, argc, argv, data_file_name, lambda);
 
   SpMat A;   // A is a row major matrix
   Vector b;
+  // load the data
   loadLibSVM(A, b, data_file_name);
   
   int problem_size = A.rows();
   int sample_size = A.cols();  
+  Vector x(problem_size, 0.);   // unknown variables, initialized to zero
+  Vector Atx(sample_size, 0.);  // maintained variables, initialized to zero
+  double operator_step_size = 0.1;
+
+  // At is needed for sync-parallel
+  SpMat At(sample_size, problem_size);
+  transpose(A, At);
+  
   params.problem_size = problem_size;
   params.tmac_step_size = 0.5;
   params.block_size = 100;
   params.worker_type = "random";  
-
-  Vector x(problem_size, 0.);   // unknown variables, initialized to zero
-  Vector Atx(sample_size, 0.);  // maintained variables, initialized to zero
-  double operator_step_size = 0.1;
   params.step_size = operator_step_size;
   print_parameters(params);
-
-  SpMat At(sample_size, problem_size);
-  transpose(A, At);
   
   prox_l1 backward(operator_step_size, lambda);
-  
   forward_grad_for_square_loss<SpMat> forward(&A, &b, &Atx, operator_step_size, 1., &At);
   ForwardBackwardSplitting<forward_grad_for_square_loss<SpMat>, prox_l1> fbs(&x, forward, backward);
 
@@ -59,7 +67,7 @@ int main(int argc, char *argv[]) {
   Atx = Vector(sample_size, 0.);
   // sync timing
   params.async = false;
-  params.step_size = 0.001;  
+  params.step_size = 0.001;  // sync-parallel has to use smaller step size
   double sync_start_time = get_wall_time();
   TMAC(fbs, params);
   double sync_end_time = get_wall_time();
@@ -75,7 +83,5 @@ int main(int argc, char *argv[]) {
 
 
 double objective(Vector& Atx, Vector& b, Vector& x, double lambda) {
-
   return lambda * norm(x, 1) + 0.5 * square_loss(x, Atx, b);
-
 }
