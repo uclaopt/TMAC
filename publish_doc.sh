8307dcb41b7b7c404d815eb9718d3b630bce4844
Zhimin Peng <zhiminp@gmail.com>
2016-07-11 11:48:05 -0700

    add script for publishing docs

---
+++ publish_doc.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+git checkout gh-pages
+rm -rf *
+touch .nojekyll
+git checkout master doc
+cd doc
+make html
+mv build/html ../
+cd ..
+rm -rf doc
+git add -A
+git commit -m 'publishing updated docs...'
+git push upstream gh-pages
+
+git checkout master
